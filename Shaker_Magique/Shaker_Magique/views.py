129cccf0f14c3acd3e0facb46e09c5f1cdb438cd
Remy Auloy <auloyremy@gmail.com>
2020-11-10 20:58:35 +0100

    remove Shaker_Magique

--- Shaker_Magique/Shaker_Magique/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def about(request):
-	return render(request,'about.html')
-def index(request):
-	return render(request,'index.html')
--- Shaker_Magique/shaker/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShakerConfig(AppConfig):
-    name = 'shaker'
