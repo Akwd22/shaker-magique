7510dce667cf4f2ac860263219f09e46d70ba5aa
ed100076 <60796682+ed100076@users.noreply.github.com>
2020-11-13 22:54:07 +0100

    Début Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Header/Navbar/Navbar";
 import PageContainer from "./components/main/PageContainer";
+import "./App.css";
 
 function App() {
   return (
