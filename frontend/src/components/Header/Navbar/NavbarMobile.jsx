4fb4e4487b712fa82d9fcb64cf1eb12dd6089416
Remy Auloy <auloyremy@gmail.com>
2020-11-14 16:07:04 +0100

    Navbar mobile + title cocktailList

--- frontend/src/components/Header/Navbar/NavbarMobile.jsx
+++ frontend/src/components/Header/Navbar/NavbarMobile.jsx
@@ -1,23 +1,34 @@
+import React from "react";
+import "./NavbarMobile.css";
+import "../../variables.css";
 
-import React from 'react'
-import "./NavbarMobile.css"
-import "../../variables.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
-
-const NavBarMobile = ({class_name, action_cross}) => {
-
-    return(
-        <div className={class_name}>
-            <div className="container-nav-mobile">
-                <div><a href="/#">Accueuil</a></div>
-                <div><a href="/#">rejoindre un hote</a></div>
-                <div><a href="/#">connexion</a></div>
-            </div>
-            <FontAwesomeIcon icon={faTimes} className="cross-icon" onClick={action_cross}/>
+const NavBarMobile = ({ class_name, action_cross }) => {
+  return (
+    <div className={class_name}>
+      <div className="container-nav-mobile">
+        <div>
+          <i className="fas fa-glass-martini-alt"></i>
+          <a href="/#">accueil</a>
         </div>
-    )
-}
+        <div>
+          <i className="fas fa-user-friends"></i>
+          <a href="/#">rejoindre un hote</a>
+        </div>
+        <div>
+          <i className="fas fa-user"></i>
+          <a href="/#">connexion</a>
+        </div>
+      </div>
+      <FontAwesomeIcon
+        icon={faTimes}
+        className="cross-icon"
+        onClick={action_cross}
+      />
+    </div>
+  );
+};
 
 export default NavBarMobile;
--- frontend/src/components/Main/HomePage/CocktailList/CocktailList.jsx
+++ frontend/src/components/Main/HomePage/CocktailList/CocktailList.jsx
@@ -6,7 +6,12 @@ class CocktailList extends React.Component {
   render() {
     return (
       <div className="cocktaillist">
+        <div className="title-page">
+        <h1>vos cocktails</h1>
+        </div>
+        <div className="list-cocktail">
         {this.props.children}
+        </div>
       </div>
     );
   }
