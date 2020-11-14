e8f2bba0a5d9e1b923b3b696a08411c8c481d281
ed100076 <60796682+ed100076@users.noreply.github.com>
2020-11-14 01:33:54 +0100

    Fusion CocktailBox/Header

---
+++ frontend/src/components/Main/HomePage/CocktailBox_GRID/CocktailBox.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./CocktailBox.css";
+import "../../../variables.css";
+import { Link } from "react-router-dom";
+
+class CocktailBox extends React.Component {
+  render() {
+    return (
+      <Link
+        style={{ color: "inherit", textDecoration: "inherit" }}
+        to="/cocktail"
+      >
+        <div className="cocktailbox">
+          <div className="cocktailbox-img"></div>
+          <div className="cocktailbox-body">
+            <h1>Titre Cocktail</h1>
+            <h2>Description</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              ut deserunt fugit. Possimus magnam incidunt nihil beatae
+              exercitationem esse corrupti, reiciendis atque suscipit, ratione
+              dolorum voluptate error ad quia commodi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              ut deserunt fugit. Possimus magnam incidunt nihil beatae
+              exercitationem esse corrupti, reiciendis atque suscipit, ratione
+              dolorum voluptate error ad quia commodi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              ut deserunt fugit. Possimus magnam incidunt nihil beatae
+              exercitationem esse corrupti, reiciendis atque suscipit, ratione
+              dolorum voluptate error ad quia commodi.
+            </p>
+          </div>
+          <div className="cocktailbox-overlay">
+            <div className="cocktail-overlay-body">
+              <i className="fas fa-book-open"></i>
+              <p>Voir la recette</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    );
+  }
+}
+
+export default CocktailBox;
---
+++ frontend/src/components/Main/HomePage/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import CocktailBox from "./CocktailBox_GRID/CocktailBox";
+import CocktailList from "./CocktailList/CocktailList";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className="page homepage">
+      <CocktailList>
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+        <CocktailBox />
+      </CocktailList>
+    </div>
+  );
+}
+
+export default HomePage;
