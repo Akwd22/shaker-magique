a2511ebab6a2211da1b6a7e8ace101339203e0e2
ed100076 <60796682+ed100076@users.noreply.github.com>
2020-11-14 16:33:21 +0100

    Modification Footer
    CSS terminé

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-                
+                <div className="background-deco"></div>
             </footer>
         )
     }
