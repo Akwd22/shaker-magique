91bed119bd8e7039d04a21f9730390e074a17cf6
Remy Auloy <auloyremy@gmail.com>
2020-11-23 10:37:01 +0100

    Titre modulable

--- frontend/src/components/Header/TitlePage/TitlePage.jsx
+++ frontend/src/components/Header/TitlePage/TitlePage.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import "./TitlePage.css"
+import React from "react";
+import "./TitlePage.css";
+import { useLocation } from "react-router-dom";
 
 function TitlePage() {
-    return (
-        <div className="title-page-header">
-            <div className="title-page-header-container">
-                <h1>Titre de la page</h1>
-            </div>
-        </div>
-    )
+  const location = useLocation();
+
+  const titlePageModifier = function () {
+    let res = "";
+    switch (location.pathname) {
+      case "/connexion":
+        res = <h1>connexion</h1>;
+        break;
+      case "/cocktail":
+        res = <h1>vos cocktails</h1>;
+        break;
+      case "/inscription":
+        res = <h1>inscription</h1>;
+        break;
+      case "/rejoindre-hote":
+        res = <h1>rejoindre un hôte</h1>;
+        break;
+      default:
+        break;
+    }
+    return res;
+  };
+
+  return (
+    <div className="title-page-header">
+      <div className="title-page-header-container">{titlePageModifier()}</div>
+    </div>
+  );
 }
 
-export default TitlePage
+export default TitlePage;
