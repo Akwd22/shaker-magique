411861a9dcd790dcb72ccb01f54d65a3cb74a980
ed100076 <60796682+ed100076@users.noreply.github.com>
2020-11-23 21:33:43 +0100

    Visuel LoginPage terminé

--- frontend/src/components/Main/LoginPage/LoginPage.jsx
+++ frontend/src/components/Main/LoginPage/LoginPage.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import "./LoginPage.css"
-import "../../variables.css"
+import React from "react";
+import "./LoginPage.css";
+import "../../variables.css";
 
 function LoginPage() {
-    return (
-        <div>
-            <h1>LoginPage</h1>
+  return (
+    <div className="page login-page">
+      <div className="lp-left-side">
+        <div className="lp-left-side-component">
+          <div className="lp-left-side-title">
+            <h1>Connexion</h1>
+          </div>
+          <div className="lp-left-side-form">
+            <input type="text" name="" id="" placeholder="Nom d'utilisateur" />
+            <input type="password" name="" id="" placeholder="Mot de passe" />
+          </div>
+          <div className="lp-left-side-remember">
+            <input type="checkbox" id="checkBoxRemember" />
+            <label htmlFor="checkBoxRemember">Se souvenir de moi</label>
+          </div>
+          <div className="lp-left-side-button">
+            <button type="submit">Se connecter</button>
+          </div>
+          <div className="lp-left-side-noaccount">
+            Vous n'avez pas de compte ?<a href="/inscription">Inscription</a>
+          </div>
         </div>
-    )
+      </div>
+      <div className="lp-right-side"></div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
