8778864ca30719f0ea00ca82d7e4fd64a72e06ba
ed100076 <60796682+ed100076@users.noreply.github.com>
2020-11-26 16:36:05 +0100

    Update settings.py

--- backend/Shaker_Magique/settings.py
+++ backend/Shaker_Magique/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 
