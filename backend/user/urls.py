61ee5aab0d79ae10031be6cf066680350cec4129
Remy Auloy <auloyremy@gmail.com>
2020-12-14 17:56:06 +0100

    Update urls.py

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,7 +4,6 @@ from .views import CustomUserCreate, BlacklistTokenUpdateView, CurentUserView
 app_name = 'user'
 
 urlpatterns = [
-    path('userdetail/<str:user_name>', UserDetail.as_view(), name="userdetail"),
     path('register/', CustomUserCreate.as_view(), name="create_user"),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(), name='blacklist'),
     path('current/', CurentUserView.as_view(), name="current_user")
