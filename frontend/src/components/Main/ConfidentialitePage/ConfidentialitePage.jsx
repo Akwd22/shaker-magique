f252c6f9bbce1e598cf1366afcfe2f2667413dc8
ed100076 <60796682+ed100076@users.noreply.github.com>
2021-01-13 16:20:55 +0100

    ConfidentialitePage

--- frontend/src/components/Main/ConfidentialitePage/ConfidentialitePage.jsx
+++ frontend/src/components/Main/ConfidentialitePage/ConfidentialitePage.jsx
@@ -5,7 +5,79 @@ export default function ConfidentialitePage() {
   return (
     <div className="page politique-confidentialite">
       <div className="politique-confidentialite-container">
-        Code HTML dans ce bloc div
+      <h3>Identité du responsable de Shaker Magique et du traitement</h3>
+        <p>Le site flotof.fr est sous la propriété de :</p>
+        <p>Florence Mendes, Florence.Mendes@iut-dijon.u-bourgogne.fr</p>
+      <h3>Respect de la vie privée et traitement des données personnelles</h3>
+      <div>
+        <p>
+            Toutes les données personnelles recueillies proviennent de l’enregistrement des coordonnées 
+             des personnes les ayant directement communiquées à Shaker Magique.</p>
+        <p
+        >Lors de l’inscription, les données personnelles de l’utilisateur sont conservées (nom, prénom, date de naissance 
+            genre, adresse mail). Seront aussi conservées dans une base de données la carte de cocktails d'un utilisateur
+            ainsi que son stock d’ingrédients. </p>
+        <p>
+            Shaker Magique garantit que les informations enregistrées sur le site sont traitées de manière 
+            strictement confidentielle. Les données que vous nous communiquez sont nécessaires au traitement 
+            de vos demandes d’information. </p>
+        <p>
+            Elles ne seront en aucun cas utilisées de quelque autre manière que ce soit, dans le cadre du site ou en-dehors.</p>
+      </div>
+      <h3>Droit d’accès, de rectification et de suppression</h3>
+      <div>
+          <p>Vous pouvez disposer des droits suivants concernant vos données personnelles :</p>
+          <ul>
+              <li>Le droit de demander l’accès à vos données personnelles. Cela vous permet de recevoir une copie des 
+                données personnelles que nous détenons à votre sujet et de vérifier que nous les traitons conformément à la loi.</li>
+                <br />
+              <li>Le droit de demander la correction de vos données personnelles si elles sont inexactes. Vous pouvez 
+                 également compléter les données personnelles incomplètes en notre possession, compte tenu des fins du traitement.</li>
+                <br />
+              <li>Le droit de demander la suppression de vos données personnelles si vos données personnelles ne sont plus 
+                nécessaires aux fins auxquelles nous les avons recueillies ou traitées.</li>
+          </ul>
+      </div>
+      <h3>Propriété intellectuelle</h3>
+      <div>
+          <p>
+            Les photographies, textes, slogans, dessins, image, séquences animées sonores ou non ainsi que toutes 
+            œuvres intégrées dans le site sont la propriété de Shaker Magique ou de tiers ayant autorisé Shaker Magique 
+            à les utiliser. Toute représentation totale ou partielle de ce site est soumise à l’autorisation préalable, 
+            écrite et expresse de Shaker Magique. Tout contrevenant s’expose aux sanctions visées aux articles L 343-1 
+            et suivants du Code de la propriété intellectuelle.
+          </p>
+        </div>
+      <h3>Les cookies</h3>
+      <h4>Définition du cookie</h4>
+      <div>
+          <p>
+            Un cookie est un petit fichier texte qui est placé ou stocké sur votre terminal (ordinateur, 
+            tablette ou appareil mobile) lorsque vous visitez un site internet. Le cookie permet à un site 
+             de vous reconnaître, de vous aider à naviguer de page en page sur un site internet, fournit 
+             des connexions sécurisées et se souvient de vos préférences lors de vos prochaines visites.
+          </p>
+      </div>
+      <h4>Suppression des cookies</h4>
+      <div>
+          <p>
+            L’utilisateur peut à tout moment désactiver les cookies enregistrés sur son terminal. Pour cela,
+            il lui suffit de sélectionner les paramètres appropriés dans son navigateur. Cependant, cette
+            désactivation aura pour conséquence d’empêcher l’accès à certaines fonctionnalités du Site
+            permettant de personnaliser les services proposés par Shaker Magique.
+          </p>
+      </div>
+      <h4>Utilisation des cookies</h4>
+      <div>
+          <p> 
+            Sur Shaker Magique, ces cookies vous éviteront de devoir fournir à chaque fois des
+            information que vous nous avez déjà communiquées dans la mesure où ils se souviendront que vous nous avez
+            communiqué ces informations à une date intérieure, cela permet donc de créer une sauvegarde de votre session.</p>
+          <p>
+            Il vous est possible de vous opposer à l’enregistrement de cookies en désactivant cette
+            fonction de votre navigateur dans les préférences de celui-ci.
+          </p>
+      </div>
       </div>
     </div>
   );
